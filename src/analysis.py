7c6c60c741f8de76997d40c00a38fa0985656c92
ilya0735 <iliaros0707@gmail.com>
2026-05-05 10:36:15 +0300

    Add 3 generators

--- src/analysis.py
+++ src/analysis.py
@@ -1,9 +1,30 @@
 from src.data_loader import df
-import pandas as pd
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-print(df.describe())
+
+def month_generator(month):
+    for i, ROW in df.iterrows():
+        if ROW['date'].month == month:
+            yield ROW
+
+def day_of_week_generator(day):
+    for i, ROW in df.iterrows():
+        if ROW['day_of_week'] == day:
+            yield ROW
+
+def sleep_quality_generator(quality):
+    for i, ROW in df.iterrows():
+        if ROW['sleep_quality'] == quality:
+            yield ROW
+
+
+for row in month_generator(month=2):
+    print(row)
+
+for row in day_of_week_generator(day='Monday'):
+    print(row)
+
+for row in sleep_quality_generator(quality='poor'):
+    print(row)
 
 
 
