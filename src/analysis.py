0472470a90b9246a7948a231d3ab10c0c656f16e
ilya0735 <iliaros0707@gmail.com>
2026-05-08 20:57:34 +0300

    Add files via upload

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,13 @@
-from src.data_loader import df
+from datetime import datetime
 
+import numpy as np
+
+from src.data_loader import df
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 def month_generator(month):
     for i, ROW in df.iterrows():
@@ -17,14 +25,48 @@ def sleep_quality_generator(quality):
             yield ROW
 
 
-for row in month_generator(month=2):
-    print(row)
-
-for row in day_of_week_generator(day='Monday'):
-    print(row)
-
-for row in sleep_quality_generator(quality='poor'):
-    print(row)
+def analytics_day_of_week_wake_up():
+    grouped_day_of_week_df = df.groupby('day_of_week').agg({
+        'wake_time': 'mean',
+        'bedtime': 'mean'
+    })
 
 
+    grouped_day_of_week_df['bedtime'] = (grouped_day_of_week_df['bedtime']
+                                         .apply(lambda x: datetime(1900, 1, 1) + x))
 
+    # Жесточайший костыль
+    max_row_bedtime = grouped_day_of_week_df[grouped_day_of_week_df['bedtime'] == np.max(grouped_day_of_week_df['bedtime'].values)]
+    max_row_wake_time = grouped_day_of_week_df[grouped_day_of_week_df['wake_time'] == np.max(grouped_day_of_week_df['wake_time'].values)]
+
+    print(max_row_bedtime)
+    print(max_row_wake_time)
+
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(8, 7))
+    ax[0].plot(grouped_day_of_week_df.index, grouped_day_of_week_df['wake_time'])
+    ax[0].set_title('время подъема')
+    ax[0].grid()
+
+    ax[1].plot(grouped_day_of_week_df.index, grouped_day_of_week_df['bedtime'])
+    ax[1].set_title('время отбоя')
+    ax[1].grid()
+
+    plt.show()
+
+def analytics_count_of_qualities():
+    grouped_quality_df = df.groupby('sleep_quality')['date'].count()
+
+    quality_dict = grouped_quality_df.to_dict()
+
+    categories = list(quality_dict.keys())
+    values = list(quality_dict.values())
+
+    plt.bar(categories, values)
+
+    plt.title('Распределение качества сна')
+    plt.xlabel('Качество сна')
+    plt.ylabel('Частота')
+    plt.show()
+
+
+analytics_day_of_week_wake_up()
