d129caabe07ee641e331dc7a5c6569c051143c49
ilya0735 <iliaros0707@gmail.com>
2026-05-09 19:04:18 +0300

    Наброски алгоритма в main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import pandas as pd
+from src.data_loader import df
+
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 21, 22, 23, 77]
 
+pd.set_option('display.max_rows', None)
 
 arr_of_arr = []
 arr_to_append = []
@@ -10,19 +14,13 @@ for i in range(0, len(arr)-1):
         arr_of_arr.append(arr_to_append)
         arr_to_append = []
 
-print(arr_of_arr)
+
+print(df.loc[(df['sleep_quality'] == 'good')
+             | (df['sleep_quality'] == 'excellent')
+             | (df['sleep_quality'] == 'average')])
 
 
 
 
-# import pandas as pd
-#
-# data = {'value': ['A', 'A', 'B', 'C', 'C', 'C', 'A', 'A', 'C', 'C']}
-# df = pd.DataFrame(data)
-#
-#
-# target = "C"
-#
-# max_c = df[df['value'] == target]
-#
-# print(max_c)
+
+
