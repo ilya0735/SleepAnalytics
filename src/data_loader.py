9af79d76beef133e866586f45f1441a75a0b6d5d
ilya0735 <iliaros0707@gmail.com>
2026-05-09 19:53:18 +0300

    Обновил data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,74 +6,77 @@ from pathlib import Path
 from src.utils import *
 
 
-@timer
-@logger
-def data_loader():
-    base = Path(__file__).resolve().parent.parent
-    path_to_data = base / 'data' / 'night_owl_final_distorted.csv'
+class DataLoader:
+    def __init__(self):
+        self.__df = self.__data_loader()
+        self.__full_check()
 
-    dataset = pd.read_csv(path_to_data)
-    return dataset
+    @timer
+    @logger
+    def __data_loader(self):
+        base = Path(__file__).resolve().parent.parent
+        path_to_data = base / 'data' / 'night_owl_final_distorted.csv'
 
-df = data_loader()
+        dataset = pd.read_csv(path_to_data)
+        return dataset
 
-def normalize(bedtime):
-    if bedtime.hour < 16:
-        return timedelta(hours=bedtime.hour, minutes=bedtime.minute)
-    else:
-        return timedelta(days=-1, hours=bedtime.hour, minutes=bedtime.minute)
+    @staticmethod
+    def __normalize(bedtime):
+        if bedtime.hour < 16:
+            return timedelta(hours=bedtime.hour, minutes=bedtime.minute)
+        else:
+            return timedelta(days=-1, hours=bedtime.hour, minutes=bedtime.minute)
 
-@timer
-@logger
-def data_formating():
-    df['date'] = pd.to_datetime(df['date'])
-    df['bedtime'] = pd.to_datetime(df['bedtime'], format='%H:%M')
-    df['wake_time'] = pd.to_datetime(df['wake_time'], format='%H:%M')
-    df['bedtime'] = df['bedtime'].apply(normalize)
-    day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    df['day_of_week'] = pd.Categorical(df['day_of_week'], categories=day_order, ordered=True)
-    quality_order = ['very_poor', 'poor', 'average', 'good', 'excellent']
-    df['sleep_quality'] = pd.Categorical(df['sleep_quality'], categories=quality_order, ordered=True)
+    @timer
+    @logger
+    def __data_formating(self):
+        self.__df['date'] = pd.to_datetime(self.__df['date'])
+        self.__df['bedtime'] = pd.to_datetime(self.__df['bedtime'], format='%H:%M')
+        self.__df['wake_time'] = pd.to_datetime(self.__df['wake_time'], format='%H:%M')
+        self.__df['bedtime'] = self.__df['bedtime'].apply(self.__normalize)
+        day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        self.__df['day_of_week'] = pd.Categorical(self.__df['day_of_week'], categories=day_order, ordered=True)
+        quality_order = ['very_poor', 'poor', 'average', 'good', 'excellent']
+        self.__df['sleep_quality'] = pd.Categorical(self.__df['sleep_quality'], categories=quality_order, ordered=True)
 
-@timer
-@logger
-def validate_df():
-    schema = pa.DataFrameSchema({
-        'date': pa.Column(str),
-        'day_of_week': pa.Column(str),
-        'bedtime': pa.Column(str),
-        'wake_time': pa.Column(str),
-        'total_sleep_hours': pa.Column(float, checks=Check.in_range(0, 25)),
-        'deep_sleep_hours': pa.Column(float, checks=Check.in_range(0, 25)),
-        'rem_sleep_hours': pa.Column(float, checks=Check.in_range(0, 25)),
-        'awakenings': pa.Column(int),
-        'sleep_quality': pa.Column(str, checks=Check.isin(['excellent', 'good', 'average', 'poor', 'very_poor'])),
-    })
-    return schema.validate(df)
+    @timer
+    @logger
+    def __validate_df(self):
+        schema = pa.DataFrameSchema({
+            'date': pa.Column(str),
+            'day_of_week': pa.Column(str),
+            'bedtime': pa.Column(str),
+            'wake_time': pa.Column(str),
+            'total_sleep_hours': pa.Column(float, checks=Check.in_range(0, 25)),
+            'deep_sleep_hours': pa.Column(float, checks=Check.in_range(0, 25)),
+            'rem_sleep_hours': pa.Column(float, checks=Check.in_range(0, 25)),
+            'awakenings': pa.Column(int),
+            'sleep_quality': pa.Column(str, checks=Check.isin(['excellent', 'good', 'average', 'poor', 'very_poor'])),
+        })
+        return schema.validate(self.__df)
 
-@timer
-@logger
-def post_preparation_validate():
-    schema = pa.DataFrameSchema({
-        'date': pa.Column(pa.DateTime),
-        'bedtime': pa.Column(pa.Timedelta),
-        'wake_time': pa.Column(pa.DateTime),
-    })
-    return schema.validate(df)
+    @timer
+    @logger
+    def __post_preparation_validate(self):
+        schema = pa.DataFrameSchema({
+            'date': pa.Column(pa.DateTime),
+            'bedtime': pa.Column(pa.Timedelta),
+            'wake_time': pa.Column(pa.DateTime),
+        })
+        return schema.validate(self.__df)
 
+    @logger
+    def __full_check(self):
+        try:
+            self.__validate_df()
+            self.__data_formating()
+            self.__post_preparation_validate()
+        except Exception as e:
+            print("Нарушена структура данных", e)
 
-try:
-    validate_df()
-    data_formating()
-    post_preparation_validate()
-except Exception as e:
-    print("Нарушена структура данных", e)
+    @property
+    def df(self):
+        return self.__df
 
-
-
-
-
-
-
-
-
+DL = DataLoader()
+df = DL.df
